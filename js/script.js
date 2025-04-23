4477fb91c364df676446b086ba44905c91d13bc9
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-04-23 18:35:03 -0300

    fasfa

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function login() {
+    const res = document.getElementById("login")
+    document.getElementById("fundo-escuro").style.display = "block";
+    if (res.style.display == 'block') {
+        res.style.display = 'none'
+    } else {
+        res.style.display = 'block'
+    }
+}
+
+function fecha() {
+    const res = document.getElementById("login")
+    document.getElementById("fundo-escuro").style.display = "none";
+    if (res.style.display == 'block') {
+        res.style.display = 'none'
+    } else {
+        res.style.display = 'block'
+    }
+}
